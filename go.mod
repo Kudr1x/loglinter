ee04dd25f17fd52cf292179ee095dc88ebd3c7b2
kudrix <max51kud@gmil.com>
2026-02-24 02:14:33 +0300

    init, add gitignore, golang-lint config and Makefile

---
+++ go.mod
@@ -0,0 +1,5 @@
+module loglinter
+
+go 1.26.0
+
+require golang.org/x/tools v0.42.0
---
+++ pkg/analyzer/analyzer.go
@@ -0,0 +1,34 @@
+package analyzer
+
+import (
+	"go/ast"
+
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
+)
+
+var Analyzer = &analysis.Analyzer{
+	Name: "loglinter",
+	Doc:  "checks log messages for specific formatting and security rules",
+	Run:  run,
+
+	Requires: []*analysis.Analyzer{inspect.Analyzer},
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+
+	nodeFilter := []ast.Node{
+		(*ast.CallExpr)(nil),
+	}
+
+	inspect.Preorder(nodeFilter, func(node ast.Node) {
+		call := node.(*ast.CallExpr)
+
+		// todo
+		_ = call
+	})
+
+	return nil, nil
+}
